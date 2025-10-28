3b54951e8faa2266937edc9ab9a76fb2042c1717
Mehmet Can Çetin <mehmet213042@ogr.duzce.edu.tr>
2025-10-28 20:43:30 +0300

    n10

---
+++ API/Controllers/CartController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class CartController(ICartService service) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<ShoppingCart>> GetCartById(string id)
+    {
+        var cart = await service.GetCartAsync(id);
+        return Ok(cart ?? new ShoppingCart { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ShoppingCart>> UpdateCart(ShoppingCart cart)
+    {
+        var updatedCart = await service.SetCartAsync(cart);
+        if (updatedCart == null) return BadRequest("Problem with cart");
+
+        return updatedCart;
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteCart(string id)
+    {
+        var result = await service.DeleteCartAsync(id);
+        if (!result) return BadRequest("Problem deleting cart");
+
+        return Ok();
+    }
+}
---
+++ Core/Entities/ShoppingCart.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.Entities;
+public class ShoppingCart
+{
+    public required string Id { get; set; }
+    public List<CartItem> Items { get; set; } = [];
+}
